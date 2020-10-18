b6b98c5eb09921dfb82b92edf0b939247f47619a
CarlosJPantoja <61593197+CarlosJPantoja@users.noreply.github.com>
2020-10-17 19:22:47 -0500

    First commit, the required folders and classes were created.

---
+++ src/ui/Menu.java
@@ -0,0 +1,25 @@
+package ui;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+import model.Management;
+
+public class Menu {
+	
+	public static Scanner sc = new Scanner(System.in);
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	
+	private Management management;
+
+	public Menu(){
+		
+	}
+	
+	public void showMenu(){
+		
+	}
+}
